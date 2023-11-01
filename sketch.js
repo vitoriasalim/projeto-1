2ad37b09a0d559d9ca6b25e3dea04715530ba65f
vitoriasalim <148975005+vitoriasalim@users.noreply.github.com>
2023-11-01 09:30:12 -0300

    adicionando arquivos html,css e js

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(400, 400);
+    background ("pink")
+  }
+  
+    function draw (){ 
+    
+    stroke ("black");
+    fill ("purple");
+  
+  if (mouseIsPressed) {
+    rect (mouseX,mouseY,20,20);
+  
+  }
+    }
+  
